a742c3fd1c83f209e4da23bdaea60fcfb8c8bdcc
Michael Graves <mg@brainfat.net>
2019-03-17 09:32:22 -0500

    Version 0.9

--- regress.sh
+++ regress.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 TN=test2
 
+TMPNAM=regress-tmp
+function die {
+	local _rc=$1; shift
+	local _msg=$*
+	echo $_msg >&2
+	rm -f $TMPNAM.*
+	exit $_rc
+}
+
 function runit {
 	local _exp=$1; shift
 	local _cmd=$*
-	local _tf=`mktemp tmp.XXXXXXX || exit 1`
+	local _tf=`mktemp $TMPNAM.XXXXXX || die 1 "runit: mktemp"`
 	local _rc
 	echo -n "Running Command: '$_cmd' "
 	$_cmd >$_tf 2>&1
@@ -15,7 +24,7 @@ function runit {
 		echo "Output:"
 		cat $_tf
 		rm -f $_tf
-		exit 1
+		die 1 ""
 	fi
 	echo "Success"
 	rm -f $_tf
@@ -23,30 +32,105 @@ function runit {
 
 function testit {
 	local _exp=$1; shift
-	local _command=$*
-	local _tf=`mktemp tmp.XXXXXXX || exit 1`
-	cmd >$_tf 2>&1
-	if [ "$_exp" != $_out ]; then
-		echo "Error running command:"
-		echo "$_cmd"
-		echo "Expected: $_exp"
-		echo -n "Got:"
+	local _cmd=$*
+	local _tf=`mktemp $TMPNAM.XXXXXXX || die 1 "testit: mktemp"`
+	local _rc
+	echo -n "Testing Command: '$_cmd' "
+	$_cmd >$_tf 2>&1
+	_rc=$?
+	if [ $_rc -ne 0 ]; then
+		echo "Error"
+		echo "Command exited with $_rc"
+		echo "Output:"
 		cat $_tf
 		rm -f $_tf
-		exit 1
+		die 1 ""
 	fi
+	_out=`cat $_tf`
+	if [ "$_exp" != "$_out" ]; then
+		echo "Error"
+		echo "Expecting $_exp"
+		echo "Got       $_out"
+		rm -f $_tf
+		die 1 ""
+	fi
+	echo "Success"
 	rm -f $_tf
-
 }
+
+# clean up from last failure
+rm -rf .$TN.ca
 
 runit 0 pca $TN init
 runit 1 pca $TN init
 
-runit 0 pca $TN list macros
-runit 0 pca $TN list policy
-runit 0 pca $TN list extension
+runit 0 pca $TN config list macros
+runit 0 pca $TN config list policy
+runit 0 pca $TN config list extension
 
-#runit 0 pca $TN create root -days 10 -bits 1024
-#runit 0 pca $TN show root
-#runit 0 pca $TN create req -name cloudy -newkey -days 5 -cn cloudy.example.com -ct NoTown -sp NoWhere -co XX -or ACME -ou Enigma -em bitbucket@example.com -san DNS:partlycloudy.example.com
+# macro / policy / extension all use the same function
+testit "HOME=.$TN.ca" pca $TN config get macro -key HOME
+testit ".$TN.ca" pca $TN config get macro -key HOME -value
+testit "TEST10=10" pca $TN config set macro -a -key TEST10 -value 10
+testit "TEST10=11" pca $TN config set macro -key TEST10 -value 11
+testit "" pca $TN config set macro -d -key TEST10
+testit "" pca $TN config get macro -key TEST10
 
+# create a root cert
+runit 0 pca $TN create root -days 10 -bits 1024 -or "testCA"
+runit 0 pca $TN show root
+testit "root-test2.crt: subject= /O=testCA" pca $TN show root -subject
+
+# create a request
+runit 0 pca $TN create req -name sunny -newkey 512 -days 5 -cn sunny.example.com -or "testCA" -san DNS=bright.example.com
+testit "sunny: subject=/O=testCA/CN=sunny.example.com" pca $TN show req -name sunny
+runit 0 pca $TN sign -name sunny
+runit 0 pca $TN show cert -name sunny
+runit 0 pca $TN show cert -name sunny -subject
+runit 0 pca $TN show cert -name sunny -expire
+runit 0 pca $TN show cert -name sunny -subjecthash
+runit 0 pca $TN show cert -name sunny -issuer
+runit 0 pca $TN show cert -name sunny -issuerhash
+
+# export PKCS12
+_of=`mktemp $TMPNAM.XXXXXXX || die 1 "mktemp"`
+runit 1 pca $TN export pkcs12 -name sunny -pass "letmein" -file $_of
+runit 0 pca $TN export pkcs12 -name sunny -pass "letmein" -file $_of -overwrite
+_tf=`mktemp $TMPNAM.XXXXXXX || die 1 "mktemp"`
+echo "letmein" > $_tf
+runit 0 pca $TN export pkcs12 -name sunny -pass file:$_tf -file $_of -overwrite
+runit 0 'echo "letemin" | pca '$TN export pkcs12 -name sunny -pass -  -overwrite
+
+# test CRL
+runit 0 pca $TN create crl
+runit 0 pca $TN show crl
+runit 0 pca $TN show crl -issuer
+runit 0 pca $TN show crl -hash
+runit 0 pca $TN show crl -fingerprint
+runit 0 pca $TN show crl -num
+runit 0 pca $TN show crl -last
+runit 0 pca $TN show crl -next
+
+# revoke cert
+runit 0 pca $TN revoke -name sunny
+# refresh crl
+runit 0 pca $TN create crl
+runit 0 pca $TN show crl
+
+# cleanup sunny
+runit 1 pca $TN delete req
+runit 0 pca $TN delete req -name sunny
+runit 0 pca $TN delete key -name sunny
+runit 0 pca $TN delete cert -name sunny
+testit "" pca $TN show cert -subject
+testit "" pca $TN show req
+
+
+# work on req options
+runit 0 pca $TN create req -name cloudy -newkey 512 -days 5 -cn cloudy.example.com -or "testCA" -ou "testDiv" -ct NoPlace -sp NoWhere -co XX -em bitbucket@cloudy.example.com -san DNS=sad.example.com
+testit "cloudy: subject=/C=XX/ST=NoWhere/L=NoPlace/O=testCA/OU=testDiv/CN=cloudy.example.com/emailAddress=bitbucket@cloudy.example.com" pca $TN show req -name cloudy
+runit 0 pca $TN sign -name cloudy -sign
+testit "CA:TRUE,pathlen:1" pca $TN 'show cert -name cloudy | egrep "CA:[TF][RA]" | tr -d " "'
+
+
+die 0 "Complete Success"
